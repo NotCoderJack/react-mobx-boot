9e6f249052786ce23402bfa6541982d71bf8fbd7
haojie <haojie@WhoIsBest.local>
2018-05-16 14:46:27 +0800

    run test

--- src/stores/AppStore.js
+++ src/stores/AppStore.js
@@ -1,24 +1,26 @@
 // app全局性状态管理
-import { observable, action, runInAction } from 'mobx';
+import {observable, action, runInAction} from 'mobx';
 import * as appApis from '../services/app';
 
 export default class AppStore {
-  constructor({ list = [] }) {
-    this.list = list;
-  }
-
-  @observable show = true;
-  @observable list = [];
-
-  @action changeShow = () => { this.show = !this.show; }
-  @action getData = async (params) => {
-    try {
-      const res = await appApis.getTopicsList(params);
-      runInAction(() => {
-        this.list = res.data;
-      });
-    } catch (e) {
-      console.error(e);
+    constructor({list = []}) {
+        this.list = list;
     }
-  }
+
+    @observable show = true;
+    @observable list = [];
+
+    @action changeShow = () => {
+        this.show = !this.show;
+    }
+    @action getData = async (params) => {
+        try {
+            const res = await appApis.getTopicsList(params);
+            runInAction(() => {
+                this.list = res.data;
+            });
+        } catch (e) {
+            console.error(e);
+        }
+    }
 }
